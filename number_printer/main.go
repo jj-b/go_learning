310378938477f298e7ef668fdba54417a6b498d3
Jennifer Bottom <jen.bottom@gmail.com>
2020-01-25 20:48:00 +0000

    Program logic almost working. Printing index of element in array when it is not divisible by 3, 5 or 3 & 5, instead of element value

--- number_printer/main.go
+++ number_printer/main.go
@@ -15,8 +15,14 @@ func main() {
 If the number is not divisible by 3, 5, or 3 and 5, print the number.
 If it meets one of the above conditions print the appropriate string*/
 	for count := 0; count <= 99; count++ {
-		if intArray[count] % 3 == 0 && intArray[count] % 5 == 0{
+	if intArray[count] % 3 == 0 && intArray[count] % 5 == 0{
 fmt.Println("foo", "bar")
+} else if intArray[count] % 3 == 0{
+fmt.Println("foo")
+} else if intArray[count] % 5 == 0{
+fmt.Println("bar")
+} else {
+fmt.Println(intArray.Get[count])
 }
 //Close loop
 	}
